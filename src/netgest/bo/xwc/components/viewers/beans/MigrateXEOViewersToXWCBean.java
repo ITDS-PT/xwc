f96070fb666145fea775271d3ad485a73bb77160
Filipe Calo <filipe.calo@itds.pt>
2010-02-12 14:22:48 +0000

    change to viewers name and location

--- src/netgest/bo/xwc/components/viewers/beans/MigrateXEOViewersToXWCBean.java
+++ src/netgest/bo/xwc/components/viewers/beans/MigrateXEOViewersToXWCBean.java
@@ -1,6 +1,7 @@
 package netgest.bo.xwc.components.viewers.beans;
 
 import java.io.File;
+import java.net.URL;
 
 import netgest.bo.boConfig;
 import netgest.bo.runtime.boObjectList;
@@ -37,12 +38,17 @@ public class MigrateXEOViewersToXWCBean extends XEOBaseList {
         if (rootDasArvores .exists() && rootDasArvores.isDirectory())
     		logText+=rootDasArvores.getAbsolutePath()+" - OK\n";
         
-    	File rootDosPacotes = new File(boConfig.getDefinitiondir());
+        File rootDosPacotes = new File(boConfig.getDefinitiondir());
     	if (rootDosPacotes.exists() && rootDosPacotes.isDirectory())
     		logText+=rootDosPacotes.getAbsolutePath()+" - OK\n";
+        
+    	URL path = Thread.currentThread().getContextClassLoader().getResource("xeo.home");
+		File file = new File(path.getFile());
+    	File destinoDoXWC = new File(file.getParentFile().getParentFile().getParentFile()+File.separator+"viewers"+File.separator+"xwcconversion");
 
-    	File destinoDoXWC = new File(boConfig.getNgtHome()+File.separator+
-    			"src"+File.separator+"xwcconversion");
+    	//Criar se não existir
+    	if (!destinoDoXWC.getParentFile().exists()) 
+    		destinoDoXWC.getParentFile().mkdir();
     	
     	//Criar se não existir
     	if (!destinoDoXWC.exists()) 
