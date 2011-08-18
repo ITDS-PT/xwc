635ccc71dbb05b380ab009c8b7b0e0264238f4c9
Pedro Rio <pedro.rio@itds.pt>
2011-08-18 10:25:09 +0000

    Adição da propriedade icon para o titulo ter facilmente um icon sem precisar de se usar uma propriedade da bean

--- src/netgest/bo/xwc/components/classic/Title.java
+++ src/netgest/bo/xwc/components/classic/Title.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import netgest.bo.xwc.components.annotations.RequiredAlways;
 import netgest.bo.xwc.components.classic.scripts.XVWScripts;
+import netgest.bo.xwc.framework.XUIBaseProperty;
 import netgest.bo.xwc.framework.XUIRenderer;
 import netgest.bo.xwc.framework.XUIStateProperty;
 import netgest.bo.xwc.framework.components.XUIComponentBase;
@@ -14,7 +15,7 @@ import netgest.bo.xwc.framework.components.XUIOutput;
  * The {@link Title} component enables 
  * the user to set the title of the viewer
  * 
- * @author João Carreira
+ * @author Joao Carreira
  *
  */
 public class Title extends XUIOutput {
@@ -24,11 +25,38 @@ public class Title extends XUIOutput {
      */
 	@RequiredAlways
     private XUIStateProperty<String> valueExpression = new XUIStateProperty<String>( "valueExpression", this );
+	
+	/**
+     * An icon to put in the ptitle (before the text message)
+     */
+	@RequiredAlways
+    private XUIBaseProperty<String> icon = new XUIBaseProperty<String>( "icon", this, "" );
+	
     /**
      * Stores the rendered value of the title
      */
     private XUIStateProperty<Object> renderedValue  = new XUIStateProperty<Object>( "renderedValue", this );
 
+    /**
+     * 
+     * Retrieve the path to the icon
+     * 
+     * @return A path to the icon
+     */
+    public String getIcon(){
+    	return icon.getValue();
+    }
+    
+    /**
+     * 
+     * Set the title icon
+     * 
+     * @param iconPath Path to the icon
+     */
+    public void setIcon(String iconPath){
+    	this.icon.setValue(iconPath);
+    }
+    
     @Override
     public boolean wasStateChanged() {
         if( !super.wasStateChanged() ) {
@@ -66,11 +94,15 @@ public class Title extends XUIOutput {
 		@Override
 		public void encodeEnd(XUIComponentBase component) throws IOException {
 			Window oWnd = (Window)component.findParentComponent(Window.class);
+			String icon = ((Title)component).getIcon();
+			String iconPath = "";
+			if (!"".equalsIgnoreCase(icon))
+				iconPath = "<img src='"+icon+"' />";
 			if( oWnd != null ) {
-				oWnd.setTitle( String.valueOf( ((Title)component).getValue() ) );
+				oWnd.setTitle( iconPath + String.valueOf( ((Title)component).getValue() ) );
 			}
 			else {
-				XVWScripts.setTitle( String.valueOf( ((Title)component).getValue() ) );
+				XVWScripts.setTitle( iconPath + String.valueOf( ((Title)component).getValue() ) );
 			}
 		}
 	}
