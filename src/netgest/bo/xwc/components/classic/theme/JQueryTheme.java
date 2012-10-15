89cad81a0d770c82fc62bbdaa5182aa3d0dac61b
Pedro Rio <pedro.rio@itds.pt>
2012-10-15 13:47:01 +0000

    Tema que inclui o Jquery

---
+++ src/netgest/bo/xwc/components/classic/theme/JQueryTheme.java
@@ -0,0 +1,49 @@
+package netgest.bo.xwc.components.classic.theme;
+
+import netgest.bo.xwc.framework.XUIScriptContext;
+import netgest.bo.xwc.framework.XUIStyleContext;
+
+public class JQueryTheme extends ExtJsTheme {
+
+	
+	public String getResourceBaseUriJquery() {
+		return "jquery-xeo/";
+	}
+
+	@Override
+	public void addStyle( XUIStyleContext styleContext ) {
+		super.addStyle( styleContext );
+		styleContext.addInclude(XUIStyleContext.POSITION_HEADER, "jquery_ui_css",
+				composeUrl(getResourceBaseUriJquery() + "css/anacom/jquery-ui-1.8.20.custom.css"));
+		styleContext.addInclude(XUIStyleContext.POSITION_HEADER, "jquery_css_xeo",
+				composeUrl("ext-xeo/css/jquery-xeo.css"));
+		/*styleContext.addInclude(XUIStyleContext.POSITION_HEADER, "jqGrid_css",
+				composeUrl("jquery-xeo/css/ui.jqgrid.css"));
+		styleContext.addInclude(XUIStyleContext.POSITION_HEADER, "multiSelect_css",
+				composeUrl("jquery-xeo/css/ui.multiselect.css"));*/
+		
+		
+	}
+
+	@Override
+	public void addScripts( XUIScriptContext scriptContext ) {
+		super.addScripts( scriptContext );
+		scriptContext.addInclude(XUIScriptContext.POSITION_HEADER, "jquery",
+				composeUrl(getResourceBaseUriJquery() + "js/jquery-1.7.2.min.js"));
+		scriptContext.addInclude(XUIScriptContext.POSITION_HEADER, "jquery-ui",
+				composeUrl(getResourceBaseUriJquery() + "js/jquery-ui-1.8.20.custom.min.js"));
+		scriptContext.addInclude(XUIScriptContext.POSITION_HEADER, "jquery-blockui",
+				composeUrl(getResourceBaseUriJquery() + "js/jquery.blockUI.js"));
+		scriptContext.addInclude(XUIScriptContext.POSITION_HEADER, "jquery-jqGridLocale",
+				composeUrl(getResourceBaseUriJquery() + "js/grid.locale-en.js"));
+		scriptContext.addInclude(XUIScriptContext.POSITION_HEADER, "jquery-ui-multi-select",
+				composeUrl(getResourceBaseUriJquery() + "js/ui.multiselect.js"));
+		scriptContext.addInclude(XUIScriptContext.POSITION_HEADER, "jquery-jqGrid",
+				composeUrl(getResourceBaseUriJquery() + "js/jquery.jqGrid.src.js"));
+		scriptContext.addInclude(XUIScriptContext.POSITION_HEADER, "jquery-xeo",
+				composeUrl(getResourceBaseUriJquery() + "jquery-xeo.js"));
+	}
+	
+	
+
+}
