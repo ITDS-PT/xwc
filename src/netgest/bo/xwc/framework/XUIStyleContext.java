88880ae1da4c081d4846556675c4daec1b52e9d3
Pedro Rio <pedro.rio@itds.pt>
2012-10-15 13:22:44 +0000

    Correcção de bug com inclusão de CSS inline

--- src/netgest/bo/xwc/framework/XUIStyleContext.java
+++ src/netgest/bo/xwc/framework/XUIStyleContext.java
@@ -1,16 +1,80 @@
 package netgest.bo.xwc.framework;
 
 import java.io.IOException;
+import java.util.Enumeration;
+
+import netgest.bo.xwc.xeo.workplaces.admin.localization.ExceptionMessage;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
-import netgest.bo.xwc.framework.XUIScriptContext.Fragment;
-import netgest.bo.xwc.xeo.workplaces.admin.localization.ExceptionMessage;
-
 public class XUIStyleContext extends XUIScriptContext {
 
-    protected void renderFragment( XUIResponseWriter w, Fragment oFragment ) throws IOException {
+	public void render( XUIResponseWriter hW, XUIResponseWriter bW, XUIResponseWriter fW ) throws IOException {
+        Enumeration<Fragment> oFragmentsEnum;
+        Fragment oFragment;
+        
+        boolean bInLineTagOpen = false;
+        
+        oFragmentsEnum  = oFragmentsVector.elements();
+        while( oFragmentsEnum.hasMoreElements() ) {
+            oFragment = oFragmentsEnum.nextElement();
+            if( oFragment.getPosition() == POSITION_HEADER ) 
+            {
+                if( oFragment.getType() == TYPE_INCLUDE ) {
+                    if( bInLineTagOpen ) {
+                        hW.write('\n');
+                        hW.endElement("link");   
+                        bInLineTagOpen = false;
+                    }
+                }
+                else if ( oFragment.getType() == TYPE_TEXT ) {
+                    /*if( !bInLineTagOpen ) {
+                        hW.write('\n');
+                        hW.startElement("style", null );
+                        hW.writeAttribute("type", "text/css", null);
+                        bInLineTagOpen = true;
+                    }*/
+                }
+                renderFragment( hW, oFragment );
+            }
+        }
+        if( bInLineTagOpen ) {
+            hW.write('\n');
+            hW.endElement("style");    
+        }
+        
+        bInLineTagOpen = false;
+        oFragmentsEnum  = oFragmentsVector.elements();
+        while( oFragmentsEnum.hasMoreElements() ) {
+            oFragment = oFragmentsEnum.nextElement();
+            if( oFragment.getPosition() == POSITION_FOOTER ) 
+            {
+                if( oFragment.getType() == TYPE_INCLUDE ) {
+                    if( bInLineTagOpen ) {
+                        fW.endElement("link");   
+                        bInLineTagOpen = false;
+                    }
+                }
+                else if ( oFragment.getType() == TYPE_TEXT ) {
+                    if( !bInLineTagOpen ) {
+                        fW.startElement("style", null );
+                        bInLineTagOpen = true;
+                    }
+                }
+                renderFragment( fW, oFragment );
+            }
+        }
+        if( bInLineTagOpen ) {
+            hW.write('\n');
+            fW.endElement("style");    
+        }
+        hW.write('\n');
+
+    }
+	
+	
+    public void renderFragment( XUIResponseWriter w, Fragment oFragment ) throws IOException {
 
         // <script type="text/javascript" src="/en/us/shared/core/2/js/js.ashx?s=Csp;shared"></script>
         XUIRequestContext oRequestContext;
@@ -26,6 +90,7 @@ public class XUIStyleContext extends XUIScriptContext {
                 w.startElement("link", null);
                 w.writeAttribute("rel","stylesheet", null );
                 w.writeAttribute("type","text/css", null );
+                w.writeAttribute("media","screen", null );
                 w.writeAttribute("href", oRequestContext.getResourceUrl( oFragment.getContent().toString() ), null );
                 w.endElement("link");
             }
