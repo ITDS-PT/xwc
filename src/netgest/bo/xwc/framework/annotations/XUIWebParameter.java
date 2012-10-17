99c170ec8838c1ca8abea98f31d7a67da0e6d57f
Pedro Rio <pedro.rio@itds.pt>
2012-10-17 14:10:26 +0000

    Alteração do nome da classe

--- src/netgest/bo/xwc/framework/annotations/XUIWebParameter.java
+++ src/netgest/bo/xwc/framework/annotations/XUIWebParameter.java
@@ -4,7 +4,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.RUNTIME)
-public @interface XUIWebParamenter {
+public @interface XUIWebParameter {
 
 	public String name();
 	public String defaultValue();
