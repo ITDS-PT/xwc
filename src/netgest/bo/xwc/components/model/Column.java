996a4ca3290f5e940034693a5ecf56c673178b85
Pedro Rio <pedro.rio@itds.pt>
2012-11-14 18:00:01 +0000

    Faltava isto no commit do BUG458

--- src/netgest/bo/xwc/components/model/Column.java
+++ src/netgest/bo/xwc/components/model/Column.java
@@ -19,5 +19,13 @@ public interface Column {
     public boolean isContentHtml();
     public String applyRenderTemplate( Object value );
     public boolean isEnableAggregate();
+    /**
+     * Whether or not the column should use the value for lov filtering
+     * (if false it should use the label) 
+     * 
+     * @return True if the value of the lov should be used for filtering and false if
+     * the label of the lov should be used
+     */
+    public boolean useValueOnLov();
     
 }
