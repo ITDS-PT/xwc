61719a7221510b1aa3f0e6757f46dfd429dcf989
Antonio Cruz <antonio.cruz@itds.pt>
2012-12-04 14:47:59 +0000

    Anotação para permitir localizar uma propriedade de um componente. Funcionará integrada com o Studio...

---
+++ src/netgest/bo/xwc/components/annotations/Localize.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package netgest.bo.xwc.components.annotations;
+
+/**
+ * 
+ * Components annotation to mark a property as localizable
+ * 
+ * @author acruz
+ *
+ */
+public @interface Localize {
+
+}
