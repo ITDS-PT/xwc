ab5e66600b1cb7bf15b822c8067d45e0586b1625
Pedro Rio <pedrorio@gmail.com>
2012-12-18 11:34:47 +0000

    Componentes que estavam em falta do último commit

---
+++ src/netgest/bo/xwc/components/classic/TemplateInclude.java
@@ -0,0 +1,93 @@
+package netgest.bo.xwc.components.classic;
+
+import java.io.IOException;
+
+import javax.faces.component.NamingContainer;
+
+import netgest.bo.xwc.components.HTMLAttr;
+import netgest.bo.xwc.components.HTMLTag;
+import netgest.bo.xwc.framework.XUIBindProperty;
+import netgest.bo.xwc.framework.XUIRenderer;
+import netgest.bo.xwc.framework.XUIRequestContext;
+import netgest.bo.xwc.framework.XUIResponseWriter;
+import netgest.bo.xwc.framework.components.XUIComponentBase;
+import netgest.bo.xwc.framework.components.XUIViewRoot;
+
+public class TemplateInclude extends XUIComponentBase implements NamingContainer {
+	
+	
+	private transient boolean				wasStateChanged;	  
+	private transient XUIViewRoot 			viewRoot;
+	
+	private XUIBindProperty<XUIViewRoot> 	view = new XUIBindProperty<XUIViewRoot>("view", this, XUIViewRoot.class );
+	
+	public void setView( String view ) {
+		this.view.setExpressionText(view);
+	}
+
+	@Override
+	public void initComponent() {
+
+		if( this.viewRoot == null )
+			this.viewRoot = view.getEvaluatedValue();
+		
+		this.getChildren().clear();
+		
+		this.getChildren().add( viewRoot );
+
+	}
+	
+	public void replaceView( XUIViewRoot viewRoot ) {
+
+		if( this.viewRoot != null ) {
+			// Dispose da view
+			this.viewRoot.dispose();
+			this.viewRoot = null;
+		}
+		
+		
+		this.viewRoot = viewRoot;
+		initComponent();
+		
+		forceRenderOnClient();
+		wasStateChanged = true;
+	}
+	
+	@Override
+	public void restoreState(Object oState) {
+		super.restoreState(oState);
+		
+		if( this.getChildCount() == 1 ) {
+			this.viewRoot = (XUIViewRoot)getChild(0);
+		}
+		
+	}
+	
+	
+	@Override
+	public StateChanged wasStateChanged2() {
+		return wasStateChanged?StateChanged.FOR_RENDER:StateChanged.NONE;
+	}
+	
+	public static class XEOHTMLRenderer extends XUIRenderer {
+	
+		@Override
+		public void encodeBegin( XUIComponentBase component ) throws IOException {
+			super.encodeBegin(component);
+			XUIResponseWriter w = XUIRequestContext.getCurrentContext().getResponseWriter();
+			w.startElement( HTMLTag.DIV , component );
+			w.writeAttribute( HTMLAttr.ID , component.getClientId(), "id" );
+		}
+	
+		@Override
+		public void encodeEnd( XUIComponentBase component ) throws IOException {
+			XUIResponseWriter w = XUIRequestContext.getCurrentContext().getResponseWriter();
+			w.endElement( HTMLTag.DIV );
+			super.encodeEnd(component);
+		}
+	
+	}
+	
+	
+
+}
