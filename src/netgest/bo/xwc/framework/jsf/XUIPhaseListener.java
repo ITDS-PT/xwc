9e886a551e6ff046af1aaf0dff0f7bc629e3e683
acruz <acruz@itds.pt>
2013-07-12 15:42:53 +0100

    Log de Erros de Inicialização no Phase Listener de JSF

--- src/netgest/bo/xwc/framework/jsf/XUIPhaseListener.java
+++ src/netgest/bo/xwc/framework/jsf/XUIPhaseListener.java
@@ -6,6 +6,8 @@ import javax.faces.event.PhaseEvent;
 import javax.faces.event.PhaseId;
 import javax.faces.event.PhaseListener;
 
+import netgest.bo.localizations.LoggerMessageLocalizer;
+import netgest.bo.system.Logger;
 import netgest.bo.xwc.framework.PackageIAcessor;
 import netgest.bo.xwc.framework.XUIApplicationContext;
 import netgest.bo.xwc.framework.XUIRequestContext;
@@ -13,6 +15,8 @@ import netgest.bo.xwc.framework.XUISessionContext;
 
 public class XUIPhaseListener implements PhaseListener {
 
+	
+	 private static Logger LOGGER = netgest.bo.system.Logger.getLogger( XUIPhaseListener.class );;
 
     /**
 	 * 
@@ -27,26 +31,30 @@ public class XUIPhaseListener implements PhaseListener {
     }
 
     public void beforePhase(PhaseEvent event) {
-
-        if( oXuiApplication == null ) {
-            oXuiApplication = new XUIApplicationContext();
-            PackageIAcessor.initApplicationContext( oXuiApplication );
-        }
-        // Check if the SessionContext exits, if not create a session context
-
-        XUISessionContext oSessionContext;
-
-        Map<String,Object> oExternalSessionMap = event.getFacesContext().getExternalContext().getSessionMap();        
-        oSessionContext = (XUISessionContext)oExternalSessionMap.get(XUISessionContext.SESSION_ATTRIBUTE_ID );
-
-        if( oSessionContext == null ) {
-            oSessionContext = new XUISessionContext();
-            oExternalSessionMap.put(XUISessionContext.SESSION_ATTRIBUTE_ID, oSessionContext );
-        }
-        
-        if( XUIRequestContext.getCurrentContext() == null ) {
-        	PackageIAcessor.createRequestContext( oXuiApplication );
-        }
+    	try {
+	        if( oXuiApplication == null ) {
+	            oXuiApplication = new XUIApplicationContext();
+	            PackageIAcessor.initApplicationContext( oXuiApplication );
+	        }
+	        // Check if the SessionContext exits, if not create a session context
+	
+	        XUISessionContext oSessionContext;
+	
+	        Map<String,Object> oExternalSessionMap = event.getFacesContext().getExternalContext().getSessionMap();        
+	        oSessionContext = (XUISessionContext)oExternalSessionMap.get(XUISessionContext.SESSION_ATTRIBUTE_ID );
+	
+	        if( oSessionContext == null ) {
+	            oSessionContext = new XUISessionContext();
+	            oExternalSessionMap.put(XUISessionContext.SESSION_ATTRIBUTE_ID, oSessionContext );
+	        }
+	        
+	        if( XUIRequestContext.getCurrentContext() == null ) {
+	        	PackageIAcessor.createRequestContext( oXuiApplication );
+	        }
+    	}
+    	catch (Throwable e) {
+            LOGGER.severe(LoggerMessageLocalizer.getMessage("UNEXPECTED_ERROR"),e);
+    	}
         
     }
 
