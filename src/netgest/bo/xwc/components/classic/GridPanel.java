c441fb43577966e7c8c5780ad2d440a11d24cb24
Pedro Rio <pedro.rio@itds.pt>
2013-12-17 12:06:36 +0000

    Quando se faz um filtro local (por string) mas que o tipo da coluna nao é string (cardid search, por exemplo), dava erro de class cast exception, agora n da erro e nao mostra esses resultados como fazendo parte do filtro. Falta nao mostrar o campo de filtro

--- src/netgest/bo/xwc/components/classic/GridPanel.java
+++ src/netgest/bo/xwc/components/classic/GridPanel.java
@@ -2406,8 +2406,16 @@ public class GridPanel extends ViewerInputSecurityBase {
 							String sDisplayValue = dataRecordConnector
 									.getAttribute(column).getDisplayValue();
 							
-							String sColValue = (String)dataRecordConnector
-							.getAttribute(column).getValue();
+							Object oColValue = dataRecordConnector
+									.getAttribute(column).getValue();
+							String sColValue = "";
+
+							if (oColValue instanceof String){
+								sColValue = oColValue.toString();
+							} else {
+								addLine = false;
+								continue ; //Not supported probably a CardID Search on a Bridge
+							}
 							
 							String sColumnValue = sDisplayValue == null ? ""
 									: sDisplayValue.toUpperCase();
