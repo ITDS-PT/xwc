cabc6b4c4ee73c72fd80d1d8f2da4422e8d52c03
Pedro Rio <pedro.rio@itds.pt>
2014-08-12 09:46:50 +0100

    Acrescentado um logger em caso de erro, para facilitar o debug

--- src/netgest/bo/xwc/framework/jsf/XUIViewHandler.java
+++ src/netgest/bo/xwc/framework/jsf/XUIViewHandler.java
@@ -358,6 +358,11 @@ public class XUIViewHandler extends XUIViewHandlerImpl {
 		    }
 		    catch (Exception e) 
 		    {
+		    	String message = e.getMessage();
+		    	if (StringUtils.isEmpty( message )){
+		    		message = "";
+		    	}
+		    	log.warn( String.format("Could not render %s because of %s", viewToRender.getViewId(), message ) , e );
 		    	throw new RuntimeException("XEOEditBean - XSLT Transformation Error", e);
 		    }
 		    finally {
